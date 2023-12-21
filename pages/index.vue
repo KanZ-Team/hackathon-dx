9d6acb353561fb0e0509c7cf675f7838929b26f5
Ata Gülalan <ata@xava.me>
2023-12-21 17:32:14 +0300

    first page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hoş geldin bebek!</h1>
+</template>
+
+<script>
+// https://vuejs.org/guide/extras/render-function.html
+export default defineComponent({
+  name: 'Index',
+  mounted() {
+    console.log('Index page mounted')
+  }
+})
+</script>
