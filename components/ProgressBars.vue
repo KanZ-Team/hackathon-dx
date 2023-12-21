07294f58f38f7b2f99018f8805fc602992a60b52
berkozcans <berk_ozcan7@hotmail.com>
2023-12-21 22:05:52 +0300

    feat: assets

--- components/ProgressBars.vue
+++ components/ProgressBars.vue
@@ -1,16 +1,37 @@
 <template>
 <div id="app">
-    <div id="progress-container">
-        <div id="progress-bar-server-load"></div>
+    <div class="side">
+        <div id="server-load">
+            <img src="@/assets/icons/server-load.svg" alt="Your Icon" />
+        </div>
+        <div  id="progress-container">
+            <div id="progress-bar-server-load"></div>
+        </div>
     </div>
-    <div id="progress-container">
-        <div id="progress-bar-temperature"></div>
+    <div class="side">
+        <div id="temperature">
+            <img src="@/assets/icons/temperature.svg" alt="Your Icon" />
+        </div>
+        <div  id="progress-container">
+            <div id="progress-bar-temperature"></div>
+        </div>
     </div>
-    <div id="progress-container">
-        <div id="progress-bar-mood"></div>
+
+    <div class="side">
+        <div id="mood">
+            <img src="@/assets/icons/mood.svg" alt="Your Icon" />
+        </div>
+        <div id="progress-container">
+            <div id="progress-bar-mood"></div>
+        </div>
     </div>
-    <div id="progress-container">
-        <div id="progress-bar-money"></div>
+    <div class="side">
+        <div id="money">
+            <img src="@/assets/icons/money.svg" alt="Your Icon" />
+        </div>
+        <div id="progress-container">
+            <div id="progress-bar-money"></div>
+        </div>
     </div>
 </div>
 </template>
@@ -62,11 +83,30 @@ export default defineComponent({
 })
 </script>
 <style scoped>
+.side {
+  display: flex;
+}
+#temperature {
+    width: 0.25rem;
+  margin-left: 0.3rem;
+}
+#money {
+    width: 0.25rem;
+  margin-left: 0.3rem;
+}
+#mood {
+    width: 0.25rem;
+  margin-left: 0.3rem;
+}
+#server-load {
+    width: 0.25rem;
+  margin-left: 0.3rem;
+}
 #progress-container {
-  width: 2.0rem; /* 1.8rem * 2 */
+  width: 1.90rem; /* 1.8rem * 2 */
   height: 0.250rem; /* 0.1875rem * 2 */
   border-radius: 0.09375rem; /* 0.9375rem / 10 */
-  margin-top: 0.2rem;
+  margin-top: 0.4rem;
   margin-left: 0.3rem;
   background-color: #ddd;
   overflow: hidden;
@@ -74,19 +114,19 @@ export default defineComponent({
 
 #progress-bar-server-load {
   height: 100%;
-  background-color: blue;
+  background-color: #28a745;
   transition: width 1s ease-in-out;
   width: 200%; /* Genişliği iki katına çıkar */
 }
 #progress-bar-temperature {
   height: 100%;
-  background-color: red;
+  background-color: #801f0d;
   transition: width 1s ease-in-out;
   width: 200%; /* Genişliği iki katına çıkar */
 }
 #progress-bar-mood {
   height: 100%;
-  background-color: purple;
+  background-color: #3498db;
   transition: width 1s ease-in-out;
   width: 200%; /* Genişliği iki katına çıkar */
 }
